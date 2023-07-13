77d9f92ba46db0eb38f21999b35e525b959fc2a0
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2023-07-13 12:14:03 +0530

    counter app completed

---
+++ src/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+
+    const [counter, setCounter]= useState(0)
+  return (
+    <>
+    <h1> A simple counter app using React</h1>
+    <div className="counter-container">
+      
+        <button className="counter-btn" onClick={()=> setCounter(counter+1)}>Increase Counter</button>
+        <span className="counter-number">{counter}</span>
+        {counter===0?<button className="counter-btn" disabled onClick={()=> setCounter(counter-1)}>Decrease Counter</button>: <button className="counter-btn" onClick={()=> setCounter(counter-1)}>Decrease Counter</button>}
+    </div>
+    </>
+  )
+}
+
+export default Counter
